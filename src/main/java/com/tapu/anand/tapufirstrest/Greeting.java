e8df519fe414d52733f0334c6b0d9a7d6180c778
tapaswini <swain.tapaswini@gmail.com>
2019-02-22 13:57:32 -0500

    Initial commit

---
+++ src/main/java/com/tapu/anand/tapufirstrest/Greeting.java
@@ -0,0 +1,25 @@
+package com.tapu.anand.tapufirstrest;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
